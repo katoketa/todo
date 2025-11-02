7cb4811c75b2d91ca088849401276401e293e157
keita kato <infotec62509@gmail.com>
2025-11-02 19:08:52 +0900

    カテゴリ一覧ページの作成：解答確認・修正後

--- src/resources/views/category.blade.php
+++ src/resources/views/category.blade.php
@@ -6,58 +6,65 @@
 
 @section('content')
 @if(session('message'))
-<div class="message__success">
-    {{ session('message') }}
+<div class="category__alert">
+
+    <div class="category__alert--success">
+        {{ session('message') }}
+    </div>
+    @endif
+    @if($errors->any())
+    <div class="category__alert--denger">
+        <ul>
+            @foreach( $errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
 </div>
-@endif
-@if($errors->any())
-<ul class="alert__error">
-    @foreach( $errors->all() as $error)
-    <li>{{ $error }}</li>
-    @endforeach
-</ul>
-@endif
 <div class="category-content">
     <form action="/categories" class="create-form" method="post">
         @csrf
-        <div class="create-form__input">
-            <input type="text" name="name" value="{{ old('name') }}">
+        <div class="create-form__item">
+            <input class="create-form__item-input" type="text" name="name" value="{{ old('name') }}">
         </div>
-        <div class="create-form__submit">
-            <button type="submit">作成</button>
+        <div class="create-form__button">
+            <button class="create-form__button-submit" type="submit">作成</button>
         </div>
     </form>
-    <table class="category-table">
-        <tr>
-            <th>category</th>
-        </tr>
-        @foreach($categories as $category)
-        <tr>
-            <td class="update-form__td">
-                <form action="/categories/update" class="update-form" method="post">
-                    @method('PATCH')
-                    @csrf
-                    <div class="update-form__input">
+    <div class="category-table">
+        <table class="category-table__inner">
+            <tr class="category-table__row">
+                <th class="category-table__header">category</th>
+            </tr>
+            @foreach($categories as $category)
+            <tr class="category-table__row">
+                <td class="category-table__item">
+                    <form action="/categories/update" class="update-form" method="post">
+                        @method('PATCH')
+                        @csrf
+                        <div class="update-form__item">
+                            <input type="hidden" name="id" value="{{ $category['id'] }}">
+                            <input class="update-form__item-input" type="text" value="{{ $category['name'] }}">
+                        </div>
+                        <div class="update-form__button">
+                            <button class="update-form__button-submit" type="submit">更新</button>
+                        </div>
+                    </form>
+                </td>
+                <td class="category-table__item category-table__item--delete">
+                    <form action="/categories/delete" class="delete-form" method="post">
+                        @method('DELETE')
+                        @csrf
                         <input type="hidden" name="id" value="{{ $category['id'] }}">
-                        <input type="text" value="{{ $category['name'] }}">
-                    </div>
-                    <div class="update-form__submit">
-                        <button type="submit">更新</button>
-                    </div>
-                </form>
-            </td>
-            <td class="delete-form__td">
-                <form action="/categories/delete" class="delete-form" method="post">
-                    @method('DELETE')
-                    @csrf
-                    <input type="hidden" name="id" value="{{ $category['id'] }}">
-                    <div class="delete-form__submit">
-                        <button type="submit">削除</button>
-                    </div>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
+                        <div class="delete-form__button">
+                            <button class="delete-form__button-submit" type="submit">削除</button>
+                        </div>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </div>
 @endsection
